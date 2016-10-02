c4e71766c2107de86744e87314489179f0579629
Steffen Uhlig <steffen@familie-uhlig.net>
2016-10-02 19:52:07 +0200

    Derive expected phrase length from description

--- spec/word_clock/stripe_spec.rb
+++ spec/word_clock/stripe_spec.rb
@@ -14,9 +14,12 @@ describe WordClock::Stripe do
   context 'es ist mitternacht' do
     let(:hour) { 0 }
     let(:minute) { 0 }
+    let(:expected_pixels){[0, 1, 3, 4, 5, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]}
+
+    phrase_length = description.tr(' ', '').length
 
     it 'has the right amount of pixels lit' do
-      expect(pixels.size).to eq(16) # TODO This is the length of the spec name sans the spaces
+      expect(pixels.size).to eq(phrase_length)
     end
 
     it 'has indices that are only increasing' do
@@ -26,16 +29,19 @@ describe WordClock::Stripe do
     end
 
     it 'has the right pixels lit' do
-      expect(pixels).to eq([0, 1, 3, 4, 5, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268])
+      expect(pixels).to eq(expected_pixels)
     end
   end
 
   context 'es ist sechs uhr vier zehn' do
     let(:hour) { 6 }
     let(:minute) { 14 }
+    let(:expected_pixels){[0, 1, 3, 4, 5, 25, 26, 27, 28, 29, 105, 106, 107, 133, 134, 135, 136, 194, 195, 196, 197]}
+
+    phrase_length = description.tr(' ', '').length
 
     it 'has the right amount of pixels lit' do
-      expect(pixels.size).to eq(21)
+      expect(pixels.size).to eq(phrase_length)
     end
 
     it 'has indices that are only increasing' do
@@ -45,7 +51,7 @@ describe WordClock::Stripe do
     end
 
     it 'has the right pixels lit' do
-      expect(pixels).to eq([0, 1, 3, 4, 5, 25, 26, 27, 28, 29, 105, 106, 107, 133, 134, 135, 136, 194, 195, 196, 197])
+      expect(pixels).to eq(expected_pixels)
     end
   end
 
