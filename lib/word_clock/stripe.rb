cdc45711f1d53315f9d841c9e62d42ba694714a4
Steffen Uhlig <steffen@familie-uhlig.net>
2016-10-03 20:41:46 +0200

    Add non-trivial cases until 02:05

--- lib/word_clock/stripe.rb
+++ lib/word_clock/stripe.rb
@@ -21,10 +21,21 @@ module WordClock
     #
     def pixels(hour, minute)
       return lookup('ES', 'IST', 'MITTERNACHT') if hour.zero? && minute.zero?
-      return lookup('ES', 'IST', 'EINE', 'MINUTE', 'NACH', 'MITTERNACHT') if hour.zero? && 1 == minute
-      return lookup('ES', 'IST', minute_words(minute), 'MINUTEN', 'NACH', 'MITTERNACHT') if hour.zero? && (2..5).cover?(minute)
+      return lookup('ES', 'IST', 'EINS', 'NACH', 'MITTERNACHT') if hour.zero? && 1 == minute # TODO obsolete?
+      return lookup('ES', 'IST', minute_words(minute), 'NACH', 'MITTERNACHT') if hour.zero? && (2..5).cover?(minute)
+      return lookup('ES', 'IST', 'FÜNF', 'NACH', 'MITTERNACHT') if hour.zero? && 5 == minute # TODO obsolete?
+      return lookup('ES', 'IST', 'FÜNF', 'NACH', 'EINS') if 1 == hour && 5 == minute
+      return lookup('ES', 'IST', 'FÜNF', 'NACH', hour_words(hour + 1)) if 13 > hour && 5 == minute
+      return lookup('ES', 'IST', 'VIERTEL', 'EINS') if hour.zero? && 15 == minute
+      return lookup('ES', 'IST', 'VIERTEL', hour_words(hour + 1)) if 13 > hour && 15 == minute
+      return lookup('ES', 'IST', 'FÜNF', 'VOR', 'HALB', 'EINS') if hour.zero? && 25 == minute
+      return lookup('ES', 'IST', 'FÜNF', 'VOR', 'HALB', hour_words(hour + 1)) if 13 > hour && 25 == minute
       return lookup('ES', 'IST', 'HALB', 'EINS') if hour.zero? && 30 == minute
       return lookup('ES', 'IST', 'HALB', hour_words(hour + 1)) if 13 > hour && 30 == minute
+      return lookup('ES', 'IST', 'ZEHN', 'VOR', 'EINS') if hour.zero? && 50 == minute
+      return lookup('ES', 'IST', 'ZEHN', 'VOR', hour_words(hour + 1)) if 13 > hour && 50 == minute
+      return lookup('ES', 'IST', 'FÜNF', 'VOR', 'EINS') if hour.zero? && 55 == minute
+      return lookup('ES', 'IST', 'FÜNF', 'VOR', hour_words(hour + 1)) if 13 > hour && 55 == minute
 
       lookup('ES', 'IST', hour_words(hour), 'UHR', minute_words(minute))
     end
@@ -130,6 +141,26 @@ module WordClock
         %w(SIEBEN UND DREISSIG),
         %w(ACHT UND DREISSIG),
         %w(NEUN UND DREISSIG),
+        'VIERZIG',
+        %w(EIN UND VIERZIG),
+        %w(ZWEI UND VIERZIG),
+        %w(DREI UND VIERZIG),
+        %w(VIER UND VIERZIG),
+        %w(FÜNF UND VIERZIG),
+        %w(SECHS UND VIERZIG),
+        %w(SIEBEN UND VIERZIG),
+        %w(ACHT UND VIERZIG),
+        %w(NEUN UND VIERZIG),
+        'FÜNFZIG',
+        %w(EIN UND FÜNFZIG),
+        %w(ZWEI UND FÜNFZIG),
+        %w(DREI UND FÜNFZIG),
+        %w(VIER UND FÜNFZIG),
+        %w(FÜNF UND FÜNFZIG),
+        %w(SECHS UND FÜNFZIG),
+        %w(SIEBEN UND FÜNFZIG),
+        %w(ACHT UND FÜNFZIG),
+        %w(NEUN UND FÜNFZIG),
       ][minute]
     end
   end
--- spec/word_clock/stripe_spec.rb
+++ spec/word_clock/stripe_spec.rb
@@ -17,43 +17,43 @@ RSpec.describe WordClock::Stripe do
     )
   end
 
-  describe 'es ist eine minute nach mitternacht' do
+  describe 'es ist eins nach mitternacht' do
     it_behaves_like_a_word_clock(
       0,
       1,
-      [0, 1, 3, 4, 5, 20, 21, 22, 23, 108, 109, 110, 111, 112, 113, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
+      [0, 1, 3, 4, 5, 14, 15, 16, 17, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
     )
   end
 
-  describe 'es ist zwei minuten nach mitternacht' do
+  describe 'es ist zwei nach mitternacht' do
     it_behaves_like_a_word_clock(
       0,
       2,
-      [0, 1, 3, 4, 5, 58, 59, 60, 61, 108, 109, 110, 111, 112, 113, 114, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
+      [0, 1, 3, 4, 5, 58, 59, 60, 61, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
     )
   end
 
-  describe 'es ist drei minuten nach mitternacht' do
+  describe 'es ist drei nach mitternacht' do
     it_behaves_like_a_word_clock(
       0,
       3,
-      [0, 1, 3, 4, 5, 18, 19, 20, 21, 108, 109, 110, 111, 112, 113, 114, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
+      [0, 1, 3, 4, 5, 18, 19, 20, 21, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
     )
   end
 
-  describe 'es ist vier minuten nach mitternacht' do
+  describe 'es ist vier nach mitternacht' do
     it_behaves_like_a_word_clock(
       0,
       4,
-      [0, 1, 3, 4, 5, 7, 8, 9, 10, 108, 109, 110, 111, 112, 113, 114, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
+      [0, 1, 3, 4, 5, 7, 8, 9, 10, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
     )
   end
 
-  describe 'es ist fünf minuten nach mitternacht' do
+  describe 'es ist fünf nach mitternacht' do
     it_behaves_like_a_word_clock(
       0,
       5,
-      [0, 1, 3, 4, 5, 38, 39, 40, 41, 108, 109, 110, 111, 112, 113, 114, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
+      [0, 1, 3, 4, 5, 38, 39, 40, 41, 122, 123, 124, 125, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268]
     )
   end
 
@@ -107,11 +107,11 @@ RSpec.describe WordClock::Stripe do
     )
   end
 
-  describe 'es ist null uhr fünf zehn' do
+  describe 'es ist viertel eins' do
     it_behaves_like_a_word_clock(
       0,
       15,
-      [0, 1, 3, 4, 5, 54, 55, 56, 57, 105, 106, 107, 162, 163, 164, 165, 194, 195, 196, 197]
+      [0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
     )
   end
 
@@ -123,7 +123,17 @@ RSpec.describe WordClock::Stripe do
     )
   end
 
-  # 00:17..00:28
+  # 00:17..00:24
+
+  describe 'es ist fünf vor halb eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      25,
+      [0, 1, 3, 4, 5, 38, 39, 40, 41, 116, 117, 118, 140, 141, 142, 143, 182, 183, 184, 185]
+    )
+  end
+
+  # 00:26..00:28
 
   describe 'es ist null uhr neun und zwanzig' do
     it_behaves_like_a_word_clock(
@@ -149,7 +159,103 @@ RSpec.describe WordClock::Stripe do
     )
   end
 
-  # 00:32..00:59
+  # 00:32..00:34
+
+  describe 'es ist fünf nach halb eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      35,
+      []
+    )
+  end
+
+  # 00:36..00:39
+
+  describe 'es ist zehn nach halb eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      40,
+      []
+    )
+  end
+
+  # 00:41..00:43
+
+  describe 'es ist null uhr vier und vierzig' do
+    it_behaves_like_a_word_clock(
+      0,
+      44,
+      [0, 1, 3, 4, 5, 54, 55, 56, 57, 105, 106, 107, 133, 134, 135, 136, 186, 187, 188, 216, 217, 218, 219, 220, 221, 222]
+    )
+  end
+
+  describe 'es ist drei viertel eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      45,
+      []
+    )
+  end
+
+  describe 'es ist null uhr sechs und vierzig' do
+    it_behaves_like_a_word_clock(
+      0,
+      46,
+      [0, 1, 3, 4, 5, 54, 55, 56, 57, 105, 106, 107, 166, 167, 168, 169, 170, 186, 187, 188, 216, 217, 218, 219, 220, 221, 222]
+    )
+  end
+
+  # 00:47..00:49
+
+  describe 'es ist zehn vor eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      50,
+      [0, 1, 3, 4, 5, 68, 69, 70, 71, 116, 117, 118, 182, 183, 184, 185]
+    )
+  end
+
+  # 00:51..00:53
+
+  describe 'es ist null uhr vier und fünfzig' do
+    it_behaves_like_a_word_clock(
+      0,
+      54,
+      [0, 1, 3, 4, 5, 54, 55, 56, 57, 105, 106, 107, 133, 134, 135, 136, 186, 187, 188, 227, 228, 229, 230, 231, 232, 233]
+    )
+  end
+
+  describe 'es ist fünf vor eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      55,
+      [0, 1, 3, 4, 5, 38, 39, 40, 41, 116, 117, 118, 182, 183, 184, 185]
+    )
+  end
+
+  describe 'es ist vier vor eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      56,
+      []
+    )
+  end
+
+  describe 'es ist zwei vor eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      58,
+      []
+    )
+  end
+
+  describe 'es ist eins vor eins' do
+    it_behaves_like_a_word_clock(
+      0,
+      59,
+      []
+    )
+  end
 
   describe 'es ist ein uhr' do
     it_behaves_like_a_word_clock(
@@ -159,7 +265,146 @@ RSpec.describe WordClock::Stripe do
     )
   end
 
-  # 01:01..01:29
+  describe 'es ist ein uhr eins' do
+    it_behaves_like_a_word_clock(
+      1,
+      1,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 182, 183, 184, 185]
+    )
+  end
+
+  describe 'es ist ein uhr zwei' do
+    it_behaves_like_a_word_clock(
+      1,
+      2,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 156, 157, 158, 159]
+    )
+  end
+
+  # 01:03..01:04
+
+  describe 'es ist fünf nach eins' do
+    it_behaves_like_a_word_clock(
+      1,
+      5,
+      [0, 1, 3, 4, 5, 38, 39, 40, 41, 122, 123, 124, 125, 182, 183, 184, 185]
+    )
+  end
+
+  describe 'es ist ein uhr sechs' do
+    it_behaves_like_a_word_clock(
+      1,
+      6,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 166, 167, 168, 169, 170]
+    )
+  end
+
+  # 01:07..01:09
+
+  describe 'es ist zehn nach eins' do
+    it_behaves_like_a_word_clock(
+      1,
+      10,
+      []
+    )
+  end
+
+  describe 'es ist ein uhr elf' do
+    it_behaves_like_a_word_clock(
+      1,
+      11,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 190, 191, 192]
+    )
+  end
+
+  # 01:12..01:13
+
+  describe 'es ist ein uhr vier zehn' do
+    it_behaves_like_a_word_clock(
+      1,
+      14,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 133, 134, 135, 136, 194, 195, 196, 197]
+    )
+  end
+
+  describe 'es ist viertel zwei' do
+    it_behaves_like_a_word_clock(
+      1,
+      15,
+      [0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 58, 59, 60, 61]
+    )
+  end
+
+  describe 'es ist ein uhr sech zehn' do
+    it_behaves_like_a_word_clock(
+      1,
+      16,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 166, 167, 168, 169, 194, 195, 196, 197]
+    )
+  end
+
+  # 01:07..01:18
+
+  describe 'es ist ein uhr neun zehn' do
+    it_behaves_like_a_word_clock(
+      1,
+      19,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 149, 150, 151, 152, 194, 195, 196, 197]
+    )
+  end
+
+  describe 'es ist zehn vor halb zwei' do
+    it_behaves_like_a_word_clock(
+      1,
+      20,
+      []
+    )
+  end
+
+  describe 'es ist ein uhr ein und zwanzig' do
+    it_behaves_like_a_word_clock(
+      1,
+      21,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 126, 127, 128, 186, 187, 188, 198, 199, 200, 201, 202, 203, 204]
+    )
+  end
+
+
+  # 01:22..01:23
+
+  describe 'es ist ein uhr vier und zwanzig' do
+    it_behaves_like_a_word_clock(
+      1,
+      24,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 133, 134, 135, 136, 186, 187, 188, 198, 199, 200, 201, 202, 203, 204]
+    )
+  end
+
+  describe 'es ist fünf vor halb zwei' do
+    it_behaves_like_a_word_clock(
+      1,
+      25,
+      [0, 1, 3, 4, 5, 38, 39, 40, 41, 116, 117, 118, 140, 141, 142, 143, 156, 157, 158, 159]
+    )
+  end
+
+  describe 'es ist ein uhr sechs und zwanzig' do
+    it_behaves_like_a_word_clock(
+      1,
+      26,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 166, 167, 168, 169, 170, 186, 187, 188, 198, 199, 200, 201, 202, 203, 204]
+    )
+  end
+
+  # 01:27..01:28
+
+  describe 'es ist ein uhr neun und zwanzig' do
+    it_behaves_like_a_word_clock(
+      1,
+      29,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 149, 150, 151, 152, 186, 187, 188, 198, 199, 200, 201, 202, 203, 204]
+    )
+  end
 
   describe 'es ist halb zwei' do
     it_behaves_like_a_word_clock(
@@ -168,6 +413,28 @@ RSpec.describe WordClock::Stripe do
       [0, 1, 3, 4, 5, 140, 141, 142, 143, 156, 157, 158, 159]
     )
   end
+
+  # 01:31..01:53
+
+  describe 'es ist ein uhr vier und fünfzig' do
+    it_behaves_like_a_word_clock(
+      1,
+      54,
+      [0, 1, 3, 4, 5, 14, 15, 16, 105, 106, 107, 133, 134, 135, 136, 186, 187, 188, 227, 228, 229, 230, 231, 232, 233]
+    )
+  end
+
+  # 01:55..02:04
+
+  describe 'es ist fünf nach zwei' do
+    it_behaves_like_a_word_clock(
+      1,
+      5,
+      [0, 1, 3, 4, 5, 38, 39, 40, 41, 122, 123, 124, 125, 182, 183, 184, 185]
+    )
+  end
+
+  # 02:06..06:13
 
   describe 'es ist sechs uhr vier zehn' do
     it_behaves_like_a_word_clock(
@@ -178,5 +445,6 @@ RSpec.describe WordClock::Stripe do
   end
 
   # 06:15..23:58
-  # 'es ist ist eine minute vor mitternacht'
+
+  # 'es ist ist eins vor mitternacht'
 end
