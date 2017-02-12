7db1d98e6a067a7b3e483ad617a0b77498f5b7fb
Steffen Uhlig <steffen@familie-uhlig.net>
2017-02-12 01:01:55 +0100

    Add day-specific matchers to CalendarFacade

--- lib/word_clock/calendar_facade.rb
+++ lib/word_clock/calendar_facade.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 module WordClock
+  class CarnivalMatcher
+    # TODO: Between 11.11. 11:11 and end of Faschingsdienstag
+    def match(time)
+      time.month == 11 && time.day == 11 && time.hour == 11 && time.min == 11
+    end
+  end
+
+  class AllDayMatcher
+    def initialize(day:, month:)
+      @day = day
+      @month = month
+    end
+
+    def match(time)
+      time.day == @day && time.month == @month
+    end
+  end
+
   class CalendarFacade
     def initialize(sampler)
       @sampler = sampler
+      @matchers = [
+        CarnivalMatcher.new,
+        AllDayMatcher.new(day: 8, month: 4)
+      ]
     end
 
     def sample(time=nil)
-      @color ||= @sampler.sample(time)
+      if @matchers.any? { |m| m.match(time) }
+        @sampler.sample(time)
+      else
+        @color ||= @sampler.sample(time)
+      end
     end
   end
 end
---
+++ spec/unit/word_clock/calendar_facade_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require 'word_clock/calendar_facade'
+
+RSpec.describe WordClock::CalendarFacade do
+  subject(:sampler) { WordClock::CalendarFacade.new(delegate) }
+  let(:delegate) { instance_double('XkcdColors') }
+
+  context 'on a regular day' do
+    let(:time) { Time.parse('1.11.2017 14:32') }
+
+    describe 'on first sample' do
+      it 'returns the a color sampled from the delegate' do
+        expect(delegate).to receive(:sample).and_return('blue')
+        expect(sampler.sample(time)).to eq('blue')
+      end
+    end
+
+    describe 'on subsequent samples' do
+      it 'returns the a color sampled from the delegate' do
+        expect(delegate).to receive(:sample).and_return('blue')
+
+        42.times do
+          expect(sampler.sample(time)).to eq('blue')
+        end
+      end
+    end
+  end
+
+  context 'right before start of carnival' do
+    let(:time) { Time.parse('11.11.2017 11:10') }
+
+    it 'returns the a color sampled from the delegate' do
+      expect(delegate).to receive(:sample).and_return('blue')
+
+      42.times do
+        expect(sampler.sample(time)).to eq('blue')
+      end
+    end
+  end
+
+  context 'at the start of carnival' do
+    let(:time) { Time.parse('11.11.2017 11:11:23') }
+
+    it 'retrieves a new color from the delegate with every sample' do
+      expect(delegate).to receive(:sample).and_return('red')
+      expect(delegate).to receive(:sample).and_return('green')
+      expect(delegate).to receive(:sample).and_return('blue')
+
+      expect(sampler.sample(time)).to eq('red')
+      expect(sampler.sample(time)).to eq('green')
+      expect(sampler.sample(time)).to eq('blue')
+    end
+  end
+
+  context 'at my 50th birthday' do
+    let(:time) { Time.parse('8.4.2020 10:15:42') }
+
+    it 'retrieves a new color from the delegate with every sample' do
+      expect(delegate).to receive(:sample).and_return('blue')
+      expect(delegate).to receive(:sample).and_return('green')
+      expect(delegate).to receive(:sample).and_return('red')
+
+      expect(sampler.sample(time)).to eq('blue')
+      expect(sampler.sample(time)).to eq('green')
+      expect(sampler.sample(time)).to eq('red')
+    end
+  end
+end
