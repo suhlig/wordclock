1a80ab4d4162f873dfa5109b02b9ae6541a7a239
Steffen Uhlig <steffen@familie-uhlig.net>
2017-02-12 23:03:13 +0100

    Separate date-bases color picker and color sampler

--- lib/word_clock/calendar_facade.rb
+++ lib/word_clock/calendar_facade.rb
@@ -1,58 +1,69 @@
 # frozen_string_literal: true
 require 'easter'
+require 'word_clock/color'
 
 module WordClock
-  class Computus
-    class << self
-      def ash_wednesday
-        AllDayMatcher.new(Easter.ash_wednesday)
-      end
+  # Delegate to the given color sampler
+  class Picker
+    extend Forwardable
+    def_delegator :@color_sampler, :sample
 
-      def shrove_monday
-        AllDayMatcher.new(Easter.easter - 48)
-      end
-
-      def shrove_tuesday
-        AllDayMatcher.new(Easter.easter - 47)
-      end
+    def initialize(color_sampler)
+      @color_sampler = color_sampler
     end
   end
 
-  class CarnivalMatcher
+  # Always return the same color
+  class StaticColorPicker
+    def initialize(color)
+      @color = color
+    end
+
+    def sample(time=nil)
+      @color
+    end
+  end
+
+  # Sample random colors with each call to select
+  class CarnivalPicker < Picker
     # TODO: use a range to express time >= "11.11. 11:11" && time <= "11.11. 23:59"
-    def match(time)
+    def match?(time)
       time.month == 11 && time.day == 11 && time.hour == 11 && time.min == 11
     end
   end
 
-  class AllDayMatcher
-    def initialize(time)
+  class AllDayPicker < Picker
+    def initialize(color_sampler, time)
+      super(color_sampler)
       @time = time
     end
 
-    def match(time)
+    def match?(time)
       time.day == @time.day && time.month == @time.month
     end
   end
 
-  class CalendarFacade
-    def initialize(sampler)
-      @sampler = sampler
-      @matchers = [
-        CarnivalMatcher.new,
-        AllDayMatcher.new(Time.parse('April 8')),
-        Computus.shrove_monday,
-        Computus.shrove_tuesday,
-        Computus.ash_wednesday,
+  class ColorPicker
+    def initialize(color_sampler)
+      @color_sampler = color_sampler
+
+      always_grey = StaticColorPicker.new(Color.new(127, 127, 127))
+
+      @samplers = [
+        CarnivalPicker.new(color_sampler),
+        AllDayPicker.new(color_sampler, Time.parse('April 8')),
+        AllDayPicker.new(color_sampler, Easter.easter - 48), # Rosenmontag
+        AllDayPicker.new(color_sampler, Easter.easter - 47), # Faschingsdienstag
+        AllDayPicker.new(always_grey,   Easter.easter - 46), # Aschermittwoch
       ]
     end
 
-    def sample(time=nil)
-      if @matchers.any? { |m| m.match(time) }
-        @sampler.sample(time)
-      else
-        @color ||= @sampler.sample(time)
+    def choose(time=nil)
+      @samplers.each do |sampler|
+        return sampler.sample(time) if sampler.match?(time)
       end
+
+      @color ||= @color_sampler.sample(time)
     end
   end
 end
--- spec/unit/word_clock/calendar_facade_spec.rb
+++ spec/unit/word_clock/calendar_facade_spec.rb
@@ -2,26 +2,26 @@
 require 'spec_helper'
 require 'word_clock/calendar_facade'
 
-RSpec.describe WordClock::CalendarFacade do
-  subject(:sampler) { WordClock::CalendarFacade.new(delegate) }
-  let(:delegate) { instance_double('XkcdColors') }
+RSpec.describe WordClock::ColorPicker do
+  subject(:color_picker) { WordClock::ColorPicker.new(color_color_picker) }
+  let(:color_color_picker) { instance_double('XkcdColors') }
 
   context 'on a regular day' do
     let(:time) { Time.parse('1.11.2017 14:32') }
 
     describe 'on first sample' do
-      it 'returns the a color sampled from the delegate' do
-        expect(delegate).to receive(:sample).and_return('blue')
-        expect(sampler.sample(time)).to eq('blue')
+      it 'returns the a color sampled from the color_color_picker' do
+        expect(color_color_picker).to receive(:sample).and_return('blue')
+        expect(color_picker.choose(time)).to eq('blue')
       end
     end
 
     describe 'on subsequent samples' do
-      it 'returns the a color sampled from the delegate' do
-        expect(delegate).to receive(:sample).and_return('blue')
+      it 'returns the a color sampled from the color_color_picker' do
+        expect(color_color_picker).to receive(:sample).and_return('blue')
 
         42.times do
-          expect(sampler.sample(time)).to eq('blue')
+          expect(color_picker.choose(time)).to eq('blue')
         end
       end
     end
@@ -30,65 +30,97 @@ RSpec.describe WordClock::CalendarFacade do
   context 'right before start of carnival' do
     let(:time) { Time.parse('11.11.2017 11:10') }
 
-    it 'returns the a color sampled from the delegate' do
-      expect(delegate).to receive(:sample).and_return('blue')
+    it 'returns the a color sampled from the color_color_picker' do
+      expect(color_color_picker).to receive(:sample).and_return('blue')
 
       42.times do
-        expect(sampler.sample(time)).to eq('blue')
+        expect(color_picker.choose(time)).to eq('blue')
       end
     end
   end
 
+  context 'at 11.11. at 11:11' do
+    let(:time) { Time.parse('11.11.2017 11:11:00') }
+
+    it 'retrieves a new color from the color_color_picker with every sample' do
+      expect(color_color_picker).to receive(:sample).and_return('red')
+      expect(color_color_picker).to receive(:sample).and_return('green')
+      expect(color_color_picker).to receive(:sample).and_return('blue')
+
+      expect(color_picker.choose(time)).to eq('red')
+      expect(color_picker.choose(time)).to eq('green')
+      expect(color_picker.choose(time)).to eq('blue')
+    end
+  end
+
   context 'at 11.11. after 11:11' do
-    let(:time) { Time.parse('11.11.2017 11:11:23') }
+    let(:time) { Time.parse('11.11.2017 23:59:59') }
 
-    it 'retrieves a new color from the delegate with every sample' do
-      expect(delegate).to receive(:sample).and_return('red')
-      expect(delegate).to receive(:sample).and_return('green')
-      expect(delegate).to receive(:sample).and_return('blue')
+    it 'retrieves a new color from the color_color_picker with every sample' do
+      expect(color_color_picker).to receive(:sample).and_return('red')
+      expect(color_color_picker).to receive(:sample).and_return('green')
+      expect(color_color_picker).to receive(:sample).and_return('blue')
 
-      expect(sampler.sample(time)).to eq('red')
-      expect(sampler.sample(time)).to eq('green')
-      expect(sampler.sample(time)).to eq('blue')
+      expect(color_picker.choose(time)).to eq('red')
+      expect(color_picker.choose(time)).to eq('green')
+      expect(color_picker.choose(time)).to eq('blue')
+    end
+  end
+
+  context 'at 12.11. after midnight' do
+    let(:time) { Time.parse('12.11.2017 0:11') }
+
+    it 'retrieves a new color from the color_color_picker with every sample' do
+      expect(color_color_picker).to receive(:sample).and_return('red')
+      expect(color_color_picker).to receive(:sample).and_return('green')
+      expect(color_color_picker).to receive(:sample).and_return('blue')
+
+      expect(color_picker.choose(time)).to eq('red')
+      expect(color_picker.choose(time)).to eq('green')
+      expect(color_picker.choose(time)).to eq('blue')
     end
   end
 
   context 'at Rosenmontag 2017' do
     let(:time) { Time.parse('27.2.2017') }
 
-    it 'retrieves a new color from the delegate with every sample' do
-      expect(delegate).to receive(:sample).and_return('red')
-      expect(delegate).to receive(:sample).and_return('green')
-      expect(delegate).to receive(:sample).and_return('blue')
+    it 'retrieves a new color from the color_color_picker with every sample' do
+      expect(color_color_picker).to receive(:sample).and_return('red')
+      expect(color_color_picker).to receive(:sample).and_return('green')
+      expect(color_color_picker).to receive(:sample).and_return('blue')
 
-      expect(sampler.sample(time)).to eq('red')
-      expect(sampler.sample(time)).to eq('green')
-      expect(sampler.sample(time)).to eq('blue')
+      expect(color_picker.choose(time)).to eq('red')
+      expect(color_picker.choose(time)).to eq('green')
+      expect(color_picker.choose(time)).to eq('blue')
     end
   end
 
   context 'at Faschingsdienstag 2017' do
     let(:time) { Time.parse('28.2.2017') }
 
-    it 'retrieves a new color from the delegate with every sample' do
-      expect(delegate).to receive(:sample).and_return('red')
-      expect(delegate).to receive(:sample).and_return('green')
-      expect(delegate).to receive(:sample).and_return('blue')
+    it 'retrieves a new color from the color_color_picker with every sample' do
+      expect(color_color_picker).to receive(:sample).and_return('red')
+      expect(color_color_picker).to receive(:sample).and_return('green')
+      expect(color_color_picker).to receive(:sample).and_return('blue')
 
-      expect(sampler.sample(time)).to eq('red')
-      expect(sampler.sample(time)).to eq('green')
-      expect(sampler.sample(time)).to eq('blue')
+      expect(color_picker.choose(time)).to eq('red')
+      expect(color_picker.choose(time)).to eq('green')
+      expect(color_picker.choose(time)).to eq('blue')
     end
   end
 
   context 'at Aschermittwoch 2017' do
     let(:time) { Time.parse('1.3.2017') }
 
-    it 'ignores the delegate and returns grey all day' do
-      expect(delegate).to_not receive(:sample)
+    it 'ignores the color_color_picker and returns grey all day' do
+      expect(color_color_picker).to_not receive(:sample)
 
-      42.times do
-        expect(sampler.sample(time)).to eq('grey')
+      23.times do
+        color = color_picker.choose(time)
+        expect(color).to be
+        expect(color.red).to eq(127)
+        expect(color.green).to eq(127)
+        expect(color.blue).to eq(127)
       end
     end
   end
@@ -96,14 +128,14 @@ RSpec.describe WordClock::CalendarFacade do
   context 'at my 50th birthday' do
     let(:time) { Time.parse('8.4.2020 10:15:42') }
 
-    it 'retrieves a new color from the delegate with every sample' do
-      expect(delegate).to receive(:sample).and_return('blue')
-      expect(delegate).to receive(:sample).and_return('green')
-      expect(delegate).to receive(:sample).and_return('red')
+    it 'retrieves a new color from the color_color_picker with every sample' do
+      expect(color_color_picker).to receive(:sample).and_return('blue')
+      expect(color_color_picker).to receive(:sample).and_return('green')
+      expect(color_color_picker).to receive(:sample).and_return('red')
 
-      expect(sampler.sample(time)).to eq('blue')
-      expect(sampler.sample(time)).to eq('green')
-      expect(sampler.sample(time)).to eq('red')
+      expect(color_picker.choose(time)).to eq('blue')
+      expect(color_picker.choose(time)).to eq('green')
+      expect(color_picker.choose(time)).to eq('red')
     end
   end
 end
