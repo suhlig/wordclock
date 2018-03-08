efaa16348c75e4cac2eaf71afd5efe54e8059b35
Steffen Uhlig <steffen@familie-uhlig.net>
2018-03-08 15:44:30 +0100

    Use Ruby 2.5.0 rescue-in-block

--- spec/unit/word_clock/stripe_spec.rb
+++ spec/unit/word_clock/stripe_spec.rb
@@ -4,11 +4,9 @@ require 'spec_helper'
 require 'shared_examples_for_word_clock'
 
 0.upto(23).each do |hour|
-  begin
-    require "word_clock/stripe_#{hour}_spec.rb"
-  rescue LoadError
-    RSpec.describe WordClock::Clock24 do
-      pending "spec for #{hour}:00..#{hour}:59"
-    end
+  require "word_clock/stripe_#{hour}_spec.rb"
+rescue LoadError
+  RSpec.describe WordClock::Clock24 do
+    pending "spec for #{hour}:00..#{hour}:59"
   end
 end
